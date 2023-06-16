f56ec68fbc0598b2061be87658b45a98b734968e
duconxom1999 <duconxom1999@gmail.com>
2023-06-16 14:02:43 +0900

    v1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Chào mừng bạn đã đến với khóa học Reactjs
         </p>
         <a
           className="App-link"
